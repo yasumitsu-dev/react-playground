229f38d5e3ef9eb23c45072f77a6bce0b46cad37
yasumitsu-dev <269399451+yasumitsu-dev@users.noreply.github.com>
2026-03-30 16:21:41 +0900

    update paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ function App() {
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <p>これは泰光のReactアプリです！</p>
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
